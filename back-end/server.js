217c5de0f88ace394ff4a88bb4caf1103f2f08a4
Breno Henrique <brenoeevil@gmail.com>
2025-10-31 00:40:32 -0300

    chore: projeto finalizado

--- back-end/server.js
+++ back-end/server.js
@@ -5,16 +5,28 @@ import cors from "cors";
 dotenv.config();
 
 const app = express();
-app.use(json);
+app.use(json());
 app.use(
   cors({
-    origin: "http://localhost:5500",
+    origin: ["http://localhost:5500", "http://127.0.0.1:5500"],
   })
 );
 
-const PORT = process.env.PORT;
+const PORT = 3333;
+const API_KEY = process.env.API_KEY;
 
-app.get("/", async (req, res) => {});
+app.get("/clima/:cidade", async (req, res) => {
+  const { cidade } = req.params;
+  const url = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${cidade}&aqi=yes`;
+
+  try {
+    const resultado = await fetch(url);
+    const dados = await resultado.json();
+    res.json(dados);
+  } catch (error) {
+    res.status(500).json({ erro: "Erro ao buscar clima" });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`O servidor está ligado na porta ${PORT}`);
--- front-end/js/script.js
+++ front-end/js/script.js
@@ -1,15 +1,56 @@
 "user strict";
 // variáveis
+const cidade = document.getElementById("cidade");
 const pesquisa = document.getElementById("input-clima");
 const icone = document.getElementById("imagem");
 const temperatura = document.getElementById("temperatura");
-const cidade = document.getElementById("cidade");
-const humidade = document.getElementById("humidade");
+const umidade = document.getElementById("umidade");
 const vento = document.getElementById("vento");
 
-function pesquisar(e) {
+function alterarCores(graus) {
+  let cor = "";
+  if (graus < 15) {
+    cor = "#00FFFF";
+  } else if (graus <= 20) {
+    cor = "#191970";
+  } else if (graus < 30) {
+    cor = "#FFFF00";
+  } else {
+    cor = "#FF6347";
+  }
+  document.documentElement.style.setProperty("--bg-color", cor);
+
+  document.documentElement.style.setProperty("--text-color", "#000");
+
+  document.documentElement.style.setProperty("--body-color", `${cor}55`);
+}
+
+async function buscarClima() {
+  const pesquisa = document.getElementById("input-clima").value;
+  const resp = await fetch(`http://localhost:3333/clima/${pesquisa}`);
+  const dados = await resp.json();
+
+  cidade.innerHTML = `
+  <span>${dados.location.name}</span>
+  `;
+  temperatura.innerHTML = `
+  <span>${Math.floor(dados.current.temp_c)}° C</span>
+  `;
+  umidade.innerHTML = `
+  <p>${dados.current.humidity}%</p>`;
+
+  vento.innerHTML = `
+  <p>${dados.current.wind_kph}Km/h</p>`;
+
+  icone.src = `${dados.current.condition.icon}`;
+
+  alterarCores(dados.current.temp_c);
+}
+
+async function pesquisar(e) {
   if (e.key === "Enter") {
-    console.log("Teste", e.target.value);
+    console.log(e.target.value);
+    await buscarClima();
   }
 }
 
